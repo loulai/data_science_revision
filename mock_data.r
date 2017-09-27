3b4552a273676862aa278a4ea30488f45228a4f6
Louise Lai <louiselai88@gmail.com>
2017-09-27 16:09:29 -0400

    creating repeating yet increasing dataframe actually going wellgit add .1

--- mock_data.r
+++ mock_data.r
@@ -1,8 +1,41 @@
 # ---- Creating data
 
 # Event-level data: an attendance log for every student in a school district
-
 # date | student_id | attendance
+
+total_days = 2
+total_students = 3
+library(lubridate)
+
+create_event_df <- function(start_date, days, students){
+  num_rows = days * students
+  
+  # initializing vectors for efficiency
+  start_date <- as.Date(start_date)
+  date <- rep(start_date, num_rows)
+  student_id <- numeric(num_rows)
+  attendance <- rep('Y', num_rows)
+  
+  initial_df <- data.frame(date, student_id, attendance)
+  
+  # DATE vector creation
+  day(start_date) <- day(start_date) - 1
+  for (i in 1:num_rows){
+    if(i%%students == 1){
+      day(start_date) = day(start_date) + 1
+    }
+    date[i] <- start_date
+  }
+  
+  # lol
+  initial_df <- data.frame(date, student_id, attendance)
+  initial_df
+}
+
+event_df2 <- data.frame(date=c('2017-01-08', '2017-01-08', '2017-01-08'), student_id=c(1001, 1002, 1003), attendance=c('Y','Y','N'))
+
+
+
 
 # Dimension-level data: a summary table with demographics for each student in the district
 # student_id | school_id | grade_level | date_of_birth | hometown
