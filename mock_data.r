1e64847360732118611bee17e3534484420d4141
Louise Lai <louiselai88@gmail.com>
2017-09-27 15:21:48 -0400

    mock data framework created

---
+++ mock_data.r
@@ -0,0 +1,23 @@
+# ---- Creating data
+
+# Event-level data: an attendance log for every student in a school district
+
+# date | student_id | attendance
+
+# Dimension-level data: a summary table with demographics for each student in the district
+# student_id | school_id | grade_level | date_of_birth | hometown
+
+# ---- Questions
+
+# What was the overall attendance rate for the school district yesterday?
+
+# Which grade level currently has the most students in this school district?
+
+# Which school had the highest attendance rate? The lowest?
+
+
+# ---- Further Mock Questions
+
+# Given timestamps of logins, figure out how many people on Facebook were active all seven days of a week on a mobile phone.
+
+# How do you determine what product in Facebook was used most by the non-employee users for the last quarter? [Required parameters will be given]
